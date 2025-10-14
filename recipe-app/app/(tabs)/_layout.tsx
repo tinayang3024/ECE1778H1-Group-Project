384ccdb3e22bd596562c99e73358456b4d0ffd7c
Tina Yang <tinayang3024@gmail.com>
2025-10-14 18:57:18 -0400

    minor screen editing, add dashboard and personal profile page

--- recipe-app/app/(tabs)/_layout.tsx
+++ recipe-app/app/(tabs)/_layout.tsx
@@ -6,14 +6,9 @@ import { Pressable } from 'react-native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { TAB_ICON_SIZE } from '@/utils/constants';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -27,10 +22,10 @@ export default function TabLayout() {
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
-        name="index"
+        name="index" // filename without the extension
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Dashboard',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="view-dashboard-outline" size={TAB_ICON_SIZE} color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -48,10 +43,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="personal"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Personal',
+          tabBarIcon: ({ color }) => <MaterialIcons name="account-circle" size={TAB_ICON_SIZE} color={color} />,
         }}
       />
     </Tabs>
